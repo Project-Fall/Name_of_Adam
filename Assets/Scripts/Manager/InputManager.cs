763081d313a879e562f0ff97911e8dc2a1f487a5
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-11-02 15:06:24 +0900

    feature : 유닛 움직임 방식 변경

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -3,21 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SerializeField]
 public class InputManager : Manager<InputManager>
 {
     public Action KeyAction = null;
+    private static Vector2 s_mousePosition;
+    public static Vector2 MousePosition { get { return s_mousePosition; } }
+    private Unit target;
 
     public void OnUpdate()
     {
-        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // 실시간 마우스 위치
+        s_mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        RaycastHit2D hit = Physics2D.Raycast(mouse, Vector2.zero);
-        if (Input.GetMouseButton(0) && hit.collider != null)
+        RaycastHit2D hit = Physics2D.Raycast(MousePosition, Vector2.zero);
+        if (Input.GetMouseButtonDown(0) && hit.collider != null)
         {
-            if(hit.collider.name == "Unit")
+            if (hit.collider.CompareTag("Unit"))
             {
-                hit.collider.gameObject.transform.position = mouse;
+                target = hit.collider.gameObject.GetComponent<Unit>();
+                target.OnTarget();
             }
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            target.OffTarget();
         }
     }
 }
