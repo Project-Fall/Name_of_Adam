6c42528c70166f66c2de979521aecb90baf8081b
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-11-02 15:08:13 +0900

    Update Unit.cs
     commit(763081d)에서 빠진 코드

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -2,7 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public class Unit : MouseInteraction
 {
     [SerializeField] public UnitData unitData;
+    [SerializeField] private bool _isTargeting;
+
+    private void Update()
+    {
+        if (_isTargeting)
+        {
+            transform.position = InputManager.MousePosition;
+        }
+    }
+
+    public void OnTarget()
+    {
+        _isTargeting = true;
+    }
+
+    public void OffTarget()
+    {
+        _isTargeting = false;
+    }
 }
